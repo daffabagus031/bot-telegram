25def9d0cb84a1bd82f7beced49e6b507be8ae91
Muhammad Daffa Bagus Muttaqin <63336347+daffabagus031@users.noreply.github.com>
2020-06-13 12:48:56 +0700

    Add files via upload

---
+++ mytoken.py
@@ -0,0 +1,43 @@
+TOKEN='1148995052:AAEmbMlyAR9YAOLnqzx_62fpZxq5ohSwAFQ'
+BAHASA = '🎈🎈 Selamat Datang 🎈🎈\n'\
+         'Pilih Bahasa (Choose language)\n'\
+        '👉 ketik /ind - untuk bahasa Indonesia 🇮🇩\n'\
+        '👉 ketik /eng - untuk bahasa Inggris 🇺🇸\n'
+SAPAE = 'Welcome ‼\n' \
+        'My name is Zuck I was made and designed by Daffa Bagus 👨🏼\n'\
+        '>>> Try <<<\n' \
+        '👉 type /pp - to see my profile picture 🖼\n' \
+        '>>> Task database connected bots <<<\n' \
+        '👉 type /datasiswa - to see student data in the database 💾\n'\
+        '>>> Help <<<\n'\
+        '👉 type /help - to know all my command 🆘\n'
+SAPAI = 'Selamat Datang ‼\n' \
+        'Nama saya Zuck saya dibuat dan dirancang oleh Daffa Bagus 👨🏼\n'\
+        '>>> Coba <<<\n'\
+        '👉 ketik /pp - untuk melihat foto profil saya 🖼\n' \
+        '>>> Bot terkoneksi dengan database <<<\n' \
+        '👉 ketik /datasiswa - untuk melihat data siswa database 💾\n'\
+        '>>> Help <<<\n'\
+        '👉 ketik /tlg - untuk mengetahui semua perintah saya 🆘\n'
+TLG = 'Ada yang bisa saya bantu ❓\n' \
+      '👉 Ketik /ind - untuk berinteraksi dengan bahasa Indonesia 🇮🇩\n' \
+      '👉 Ketik /eng - untuk berinteraksi dengan bahasa Inggris 🇺🇸\n' \
+      '👉 Ketik /tentang - untuk mengetahui tentang saya 📰\n' \
+      '👉 Ketik /pp - untuk melihat foto profil 🖼\n' \
+      '👉 Ketik /datasiswa - untuk tugas (koneksi ke database)\n'
+HELP = 'Is there anything I can help ❓\n' \
+      '👉 type /ind - to interact with Bahasa Indonesia 🇮🇩\n' \
+      '👉 type /eng - to interact with English 🇺🇸\n' \
+      '👉 type /about - to know more about me 📰\n' \
+      '👉 type /pp - to see my profile picture 🖼\n' \
+      '👉 type /datasiswa - for assignments (connected with database)\n'
+TENTANG = ">>> Tentang Saya <<<\n"\
+          "Saya adalah bot yang dibuat oleh Daffa Bagus 🤖 \n" \
+          "Saya diberi nama 'Zuck' oleh developer saya \nkarena ia terinspirasi dengan Mark Zuckerberg\n" \
+          "Saya diciptakan pada 13 Juni 2020\n"\
+          "Saya dibuat dengan menggunakan bahasa pemrograman Python 3 🐍 \n"
+ABOUT = ">>> About Me <<<\n"\
+        "I am a bot made by Daffa Bagus 🤖 \n" \
+        "I was named 'Zuck' by my developer \nbecause he was inspired by Mark Zuckerberg\n" \
+        "I was created on June 13, 2020\n" \
+        "I made using the Python 3 programming language 🐍\n"
